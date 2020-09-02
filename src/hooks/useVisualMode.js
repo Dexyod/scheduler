de4a66df073603a73b4a6b1d4a19bea8d25e6fc1
Declan O'Donnell <dexyod43@gmail.com>
2020-09-02 16:08:10 -0400

    added custom hook for visual mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace) => {
+    setMode(newMode);
+    replace ?
+      setHistory(prev => [newMode, ...(prev.slice(1))])
+      :
+      setHistory(prev => [newMode, ...prev]);
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[1]);
+      setHistory(prev => prev.slice(1));
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  }
+}
