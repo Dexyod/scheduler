a80892303a315aae5e668d663b50ee188f66b36f
Declan O'Donnell <dexyod43@gmail.com>
2020-09-03 16:55:18 -0400

    added on delete placeholder function

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
 export default function Show(props) {
-  const { student, interviewer, onEdit, onDelete } = props
+  function onDelete() {
+    props.onDelete(props.student.name, props.interviewer);
+  }
+
+  function onEdit() {
+    props.onEdit(props.student.name, props.interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">{student}</h2>
+        <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewer.name}</h3>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
