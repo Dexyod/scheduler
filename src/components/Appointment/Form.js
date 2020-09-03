3872cabc900efb7070c69c86013308f947539dba
Declan O'Donnell <dexyod43@gmail.com>
2020-09-03 16:55:47 -0400

    updated clearer slectedInterview by ID labeling

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,7 +4,7 @@ import Button from '../Button';
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [interviewer, setInterviewer] = useState(props.selectedInterviewID || null);
   const [error, setError] = useState('');
 
   const reset = () => {
@@ -29,22 +29,28 @@ export default function Form(props) {
     setError("");
     props.onSave(name, interviewer);
   }
-
+  function nameStateChange(event) {
+    setName(event.target.value);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={name}
+            name="name"
+            value={name}
             type="text"
             placeholder="Enter Student Name"
-            onChange={(e) => setName(e.target.value)}
-            onSubmit={(e) => e.preventDefault()}
+            onChange={nameStateChange}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList
+          interviewers={props.interviewers}
+          selectedInterviewID={interviewer}
+          setInterviewer={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
