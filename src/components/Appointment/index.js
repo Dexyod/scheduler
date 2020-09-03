a230f0a0649e8bc6d234cc725551793101853ff7
Declan O'Donnell <dexyod43@gmail.com>
2020-09-03 16:56:38 -0400

    added placeholder constanants and the save function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React, { useEffect } from 'react';
+import useVisualMode from '../../hooks/useVisualMode';
 import './styles.scss';
 
 import Header from './Header';
@@ -9,17 +10,57 @@ import Error from './Error';
 import Confirm from './Confirm';
 import Form from './Form';
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+const EDIT = "EDIT";
+const CONFIRM = "CONFIRM";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview).then(() => transition(SHOW))
+      .catch(err => console.log(err));
+
+
+  }
+
+  useEffect(() => {
+    if (props.interview && mode === EMPTY) {
+      transition(SHOW);
+    }
+    if (props.interview === null && mode === SHOW) {
+      transition(EMPTY);
+    }
+  }, [props.interview, transition, mode])
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
-        <Show
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onCancel={back}
+          onSave={save}
+        />)}
+      {mode === SHOW && props.interview &&
+        (<Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        /> :
-        <Empty />}
-
+          onEdit={() => transition(EDIT)}
+          onDelete={() => transition(CONFIRM)}
+        />)
+      }
+      {mode === "SAVING" && <Status message="Saving" />}
     </article>
   )
 }
