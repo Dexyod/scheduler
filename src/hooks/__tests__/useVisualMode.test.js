c9e52eca46abe53ced326e4eee23c074c9e19ea3
Declan O'Donnell <dexyod43@gmail.com>
2020-09-07 12:55:16 -0400

    changed test. for it. for consistency in testing

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -3,7 +3,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 
-test("useVisualMode should initialize with default value", () => {
+it("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
@@ -11,7 +11,7 @@ test("useVisualMode should initialize with default value", () => {
 
 const SECOND = "SECOND";
 
-test("useVisualMode should transition to another mode", () => {
+it("useVisualMode should transition to another mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
@@ -20,7 +20,7 @@ test("useVisualMode should transition to another mode", () => {
 
 const THIRD = "THIRD";
 
-test("useVisualMode should return to previous mode", () => {
+it("useVisualMode should return to previous mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
@@ -36,14 +36,14 @@ test("useVisualMode should return to previous mode", () => {
   expect(result.current.mode).toBe(FIRST);
 });
 
-test("useVisualMode should not return to previous mode if already at initial", () => {
+it("useVisualMode should not return to previous mode if already at initial", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
 });
 
-test("useVisualMode should replace the current mode", () => {
+it("useVisualMode should replace the current mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
