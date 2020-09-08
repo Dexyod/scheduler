f618a76a5a14a3d35224a47674b764dfdd8dff47
Declan O'Donnell <dexyod43@gmail.com>
2020-09-08 16:18:28 -0400

    Typo. changed 8pm to 5pm

--- src/components/Application.js
+++ src/components/Application.js
@@ -62,7 +62,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {schedule}
-        <Appointment key="last" time="8pm" />
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
